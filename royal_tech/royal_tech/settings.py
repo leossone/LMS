4c69fc9fe6c8b974b3891e3f1bb42239a93f7d69
Yan Santos <yan.santosf@gmail.com>
2017-11-20 14:07:17 -0200

    Projeto rodando, urls e view index configurados.

--- royal_tech/royal_tech/settings.py
+++ royal_tech/royal_tech/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'main',
 ]
 
 MIDDLEWARE = [
@@ -107,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS= (
+	os.path.join(BASE_DIR, "main/Templates/static/"),
+)
