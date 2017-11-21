394b7aa2bd8e178019b1efa07f99b9488fafda44
Yan Santos <yan.santosf@gmail.com>
2017-11-21 12:27:09 -0200

    Refatoração do código

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\yangx_000\desktop\lms\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- royal_tech/main/Templates/static/js/jsmenu.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('#nav-menu').click(function(){
-        $('ul.nav-list').addClass('open').slideToggle('200');
-    });
-});
